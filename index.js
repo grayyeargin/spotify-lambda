f7e972b8d28699e4e552b287c656439c62287e1b
grayyeargin <grayyeargin@gmail.com>
2018-10-05 15:20:34 -0400

    Add initial lambda handler and package dependencies

---
+++ index.js
@@ -0,0 +1,13 @@
+const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: process.env.REFRESH_TOKEN
+    },
+    json: true
+}
+
+exports.handler = async (event) => {
+    
+};
