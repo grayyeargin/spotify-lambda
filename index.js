cefdbd03b4a4e389121b8821b061c6bd7eb19ae6
grayyeargin <grayyeargin@gmail.com>
2018-10-05 15:41:41 -0400

    create requests to spotify api

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const authOptions = {
+const request = require('request')
+
+const tokenOptions = {
     url: 'https://accounts.spotify.com/api/token',
     headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
     form: {
-      grant_type: 'refresh_token',
-      refresh_token: process.env.REFRESH_TOKEN
+      	grant_type: 'refresh_token',
+      	refresh_token: process.env.REFRESH_TOKEN
     },
     json: true
 }
 
+const getAccessToken = () => {
+	return new Promise(function (res, rej) {
+		request.post(authOptions, function(error, response, body) {
+		    if (!error && response.statusCode === 200) {
+		      	res(body.access_token)
+		    } else {
+		    	rej(error)
+		    }
+		})
+	}
+}
+
+const promiseRequest = (options) => {
+	return new Promise(function (res, rej) {
+		request.get(options, function(error, response, body) {
+		    if (!error && response.statusCode === 200) {
+		      	res(body)
+		    } else {
+		    	rej(error)
+		    }
+		})
+	}
+}
+
 exports.handler = async (event) => {
-    
-};
+    const accessToken = await getAccessToken()
+    const me = await promiseRequest({
+	  	url: 'https://api.spotify.com/v1/me',
+	  	headers: { 'Authorization': 'Bearer ' + accessToken },
+	  	json: true
+	})
+
+	return me
+}
