5b9956147c241e5bb945d66b9176ff74fd205d18
grayyeargin <grayyeargin@gmail.com>
2018-10-07 21:33:08 -0400

    get old tracks, remove from current and add to archived

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const request = require('request')
 
 const tokenOptions = {
-    url: 'https://accounts.spotify.com/api/token',
+    url: `https://accounts.spotify.com/api/token`,
     headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
     form: {
-      	grant_type: 'refresh_token',
+      	grant_type: `refresh_token`,
       	refresh_token: process.env.REFRESH_TOKEN
     },
     json: true
@@ -12,14 +12,14 @@ const tokenOptions = {
 
 const getAccessToken = () => {
 	return new Promise(function (res, rej) {
-		request.post(authOptions, function(error, response, body) {
+		request.post(tokenOptions, function(error, response, body) {
 		    if (!error && response.statusCode === 200) {
 		      	res(body.access_token)
 		    } else {
 		    	rej(error)
 		    }
 		})
-	}
+	})
 }
 
 const promiseRequest = (options) => {
@@ -31,16 +31,46 @@ const promiseRequest = (options) => {
 		    	rej(error)
 		    }
 		})
-	}
+	})
 }
 
-exports.handler = async (event) => {
-    const accessToken = await getAccessToken()
-    const me = await promiseRequest({
-	  	url: 'https://api.spotify.com/v1/me',
-	  	headers: { 'Authorization': 'Bearer ' + accessToken },
-	  	json: true
+exports.handler = async () => {
+	const accessToken = await getAccessToken()
+
+	const playlist = await promiseRequest({
+		url: `https://api.spotify.com/v1/playlists/${process.env.CURRENT_PL}/tracks`,
+		headers: { 'Authorization': `Bearer ${accessToken}` },
+		json: true
 	})
 
-	return me
+	const expireAfterDays = 90
+	const expiredTime = new Date(+ new Date() - 60 * 60 * 24 * 1000 * expireAfterDays).getTime()
+	const oldTracks = playlist.items.filter(track => new Date(track.added_at).getTime() < expiredTime)
+	const uris = oldTracks.map(track => track.track.uri)
+
+	await new Promise(function (res, rej) {
+		request.del({
+			url: `https://api.spotify.com/v1/playlists/${process.env.CURRENT_PL}/tracks`,
+			headers: { 'Authorization': `Bearer ${accessToken}` },
+			body: JSON.stringify({
+				tracks: uris.map(uri => ({uri}))
+		  	})
+		}, function(error, response, body) {
+			if (!error && response.statusCode === 200) res(body)
+			rej(error)
+		})
+	})
+
+	await new Promise(function (res, rej) {
+		request.post({
+			url: `https://api.spotify.com/v1/playlists/${process.env.ARCHIVED_PL}/tracks`,
+			headers: { 'Authorization': `Bearer ${accessToken}` },
+			body: JSON.stringify({
+				uris: uris
+			})
+		}, function(error, response, body) {
+			if (!error && response.statusCode === 200) res(body)
+			rej(error)
+		})
+	})
 }
