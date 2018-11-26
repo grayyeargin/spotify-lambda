d2342180fc89d6938f0e359e42b17a3fe26a001e
grayyeargin <grayyeargin@gmail.com>
2018-11-26 14:49:45 -0500

    update with dotenv variables and README

---
+++ README.md
@@ -0,0 +1,11 @@
+## Fresh Spotify Playlist
+Keep a spotify playlist fresh by setting a limit on the number of days a song will stay on the playlist and archive expired songs in a separate list.
+
+### Getting Started
+1. run `npm install`
+2. run `npm run dotenv`
+3. Add spotify credentials to .env file (Client ID, Client Secret, Refresh Token, Playlist IDs etc.)
+4. run the script `node index.js`
+
+### Setting up on AWS Lambda and Cloudwatch
+This is intended to be set up as a Lambda function and run periodically with a Cloudwatch rule in [AWS](https://aws.amazon.com/)
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const request = require('request')
 
+require('dotenv').config()
+
+const {CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, CURRENT_PL, ARCHIVED_PL, DAYS_TO_EXPIRE} = process.env
+
 const tokenOptions = {
     url: `https://accounts.spotify.com/api/token`,
-    headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
+    headers: { 'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')) },
     form: {
       	grant_type: `refresh_token`,
-      	refresh_token: process.env.REFRESH_TOKEN
+      	refresh_token: REFRESH_TOKEN
     },
     json: true
 }
@@ -38,38 +42,38 @@ exports.handler = async () => {
 	const accessToken = await getAccessToken()
 
 	const playlist = await promiseRequest({
-		url: `https://api.spotify.com/v1/playlists/${process.env.CURRENT_PL}/tracks`,
+		url: `https://api.spotify.com/v1/playlists/${CURRENT_PL}/tracks`,
 		headers: { 'Authorization': `Bearer ${accessToken}` },
 		json: true
 	})
 
-	const expireAfterDays = 90
-	const expiredTime = new Date(+ new Date() - 60 * 60 * 24 * 1000 * expireAfterDays).getTime()
+	const expiredTime = new Date(+ new Date() - 60 * 60 * 24 * 1000 * parseInt(DAYS_TO_EXPIRE)).getTime()
 	const oldTracks = playlist.items.filter(track => new Date(track.added_at).getTime() < expiredTime)
-	const uris = oldTracks.map(track => track.track.uri)
+	const uris = oldTracks.filter(track => !track.track.is_local).map(track => track.track.uri)
 
+    console.log(uris)
 	await new Promise(function (res, rej) {
 		request.del({
-			url: `https://api.spotify.com/v1/playlists/${process.env.CURRENT_PL}/tracks`,
+			url: `https://api.spotify.com/v1/playlists/${CURRENT_PL}/tracks`,
 			headers: { 'Authorization': `Bearer ${accessToken}` },
 			body: JSON.stringify({
 				tracks: uris.map(uri => ({uri}))
 		  	})
 		}, function(error, response, body) {
-			if (!error && response.statusCode === 200) res(body)
+			if (!error && response.statusCode === 200) return res(body)
 			rej(error)
 		})
 	})
 
 	await new Promise(function (res, rej) {
 		request.post({
-			url: `https://api.spotify.com/v1/playlists/${process.env.ARCHIVED_PL}/tracks`,
+			url: `https://api.spotify.com/v1/playlists/${ARCHIVED_PL}/tracks`,
 			headers: { 'Authorization': `Bearer ${accessToken}` },
 			body: JSON.stringify({
 				uris: uris
 			})
 		}, function(error, response, body) {
-			if (!error && response.statusCode === 200) res(body)
+			if (!error && response.statusCode === 201) return res(body)
 			rej(error)
 		})
 	})
